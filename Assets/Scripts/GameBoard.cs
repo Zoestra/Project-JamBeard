1d221daf8b18f159c32a9639292624443be5346a
Zoestra <79121105+Zoestra@users.noreply.github.com>
2025-07-19 19:42:01 -0700

    added Place_Stone and Board_State

--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -1,10 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameBoard : MonoBehaviour
 {
-
     public int Board_Size = 19;
-    
+    public enum BoardCell{white, black, destroyed}
+    public Dictionary<Vector2, BoardCell> Board_State;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,5 +17,19 @@ public class GameBoard : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+    public bool Place_Stone(Vector2 target, int player){ /// player: 0=white, 1=black
+        if (Board_State.ContainsKey(target)){
+            return false;
+        }
+        else if(target.x > Board_Size-1 || target.y > Board_Size-1){
+            return false;
+        }
+        else{
+            Board_State.Add(target, (BoardCell)player);
+            return true;
+        }
     }
 }
