468d7a93b4853a47aedd5b05e2455c298d5385d5
Zoestra <79121105+Zoestra@users.noreply.github.com>
2025-07-19 18:53:54 -0700

    added new assets for grid and board from kayla

--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class GameBoard : MonoBehaviour
 {
+
+    public int Board_Size = 19;
+    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
