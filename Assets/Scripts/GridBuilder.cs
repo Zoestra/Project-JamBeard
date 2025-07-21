302b4f1cd8de71244c4f295d799e74f1396bf2d7
Zoestra <79121105+Zoestra@users.noreply.github.com>
2025-07-21 01:11:41 -0700

    completed dynamic board drawing

--- Assets/Scripts/GridBuilder.cs
+++ Assets/Scripts/GridBuilder.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -12,17 +13,18 @@ public class GridBuilder : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        TileDict = new Dictionary<string, Tile>{
-            {"DL-corner", GridTiles[0]},
-            {"DR_corner", GridTiles[1]},
-            {"UL_corner", GridTiles[2]},
-            {"UR_corner", GridTiles[3]},
-            {"cross"    , GridTiles[4]},
-            {"D_edge"   , GridTiles[5]},
-            {"L_edge"   , GridTiles[6]},
-            {"R_edge"   , GridTiles[7]},
-            {"U_edge"   , GridTiles[8]},
-            {"star"     , GridTiles[9]}
+        TileDict = new Dictionary<string, Tile>
+        {
+            {"DLcorner", GridTiles[0]},
+            {"DRcorner", GridTiles[1]},
+            {"ULcorner", GridTiles[2]},
+            {"URcorner", GridTiles[3]},
+            {"cross"   , GridTiles[4]},
+            {"Dedge"   , GridTiles[5]},
+            {"Ledge"   , GridTiles[6]},
+            {"Redge"   , GridTiles[7]},
+            {"Uedge"   , GridTiles[8]},
+            {"star"    , GridTiles[9]}
         };
 
         DrawGrid();
@@ -33,9 +35,81 @@ public class GridBuilder : MonoBehaviour
         int boardSize = GameObject.Find("GameBoard").GetComponent<GameBoard>().Board_Size;
         Tilemap tilemap = GetComponentInParent<Tilemap>();
         
-        tilemap.SetTile(new Vector3Int(0,0,0), TileDict["star"]);
-        tilemap.SetTile(new Vector3Int(1,1,0), TileDict["cross"]);
-        tilemap.SetTile(new Vector3Int(1,-1,0), TileDict["DL-corner"]);
+        int max  = boardSize / 2;
+        int min  = -max;
+        int star = max - 3;
+
+
+        for (int x = min; x <= max; x++)
+        {
+            for (int y = min; y <= max; y++)
+            {
+                // left edge
+                if (x == min)
+                {
+                    if (y == min)
+                    {
+                        tilemap.SetTile(new Vector3Int(x,y,0), TileDict["DLcorner"]);
+                    }
+                    else if (y == max)
+                    {
+                        tilemap.SetTile(new Vector3Int(x,y,0), TileDict["ULcorner"]);
+                    }
+                    else
+                    {
+                        tilemap.SetTile(new Vector3Int(x,y,0), TileDict["Ledge"]);
+                    }
+                }
+                
+                // center
+                if (x > min && x < max)
+                {
+                    if (y == min)
+                    {
+                        tilemap.SetTile(new Vector3Int(x,y,0), TileDict["Dedge"]);
+                    }
+                    else if (y == max)
+                    {
+                        tilemap.SetTile(new Vector3Int(x,y,0), TileDict["Uedge"]);
+                    }
+                    else
+                    {
+                        tilemap.SetTile(new Vector3Int(x,y,0), TileDict["cross"]);
+                    }
+                    // stars
+                    if ( Math.Abs(x) == star || x == 0)
+                    {
+                        if ( Math.Abs(y) == star || y == 0)
+                        {
+                            tilemap.SetTile(new Vector3Int(x,y,0), TileDict["star"]);
+                        }
+                    }
+
+
+                }
+
+                // right edge
+                if (x == max)
+                {
+                    if (y == min)
+                    {
+                        tilemap.SetTile(new Vector3Int(x,y,0), TileDict["DRcorner"]);
+                    }
+                    else if (y == max)
+                    {
+                        tilemap.SetTile(new Vector3Int(x,y,0), TileDict["URcorner"]);
+                    }
+                    else
+                    {
+                        tilemap.SetTile(new Vector3Int(x,y,0), TileDict["Redge"]);
+                    }
+                }
+
+               
+                
+            }
+        }
+
         
 
     }
