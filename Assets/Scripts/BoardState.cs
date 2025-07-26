bdc5f79d323e4839ceadc2712590c490c96218a1
Zoestra <79121105+Zoestra@users.noreply.github.com>
2025-07-26 14:10:07 -0700

    Juice: expand board

--- Assets/Scripts/BoardState.cs
+++ Assets/Scripts/BoardState.cs
@@ -8,7 +8,8 @@ using UnityEngine.Tilemaps;
 public enum BoardCell{white, black, destroyed}
 public class BoardState: MonoBehaviour
 {
-    public int board_size = 19;
+    private List<int> _BoardSizes = new(){9, 13, 19, 31};
+    private int _board_size = 2;
     public Dictionary<Vector3Int, BoardCell> board_state = new();
 
     [SerializeField]
@@ -35,7 +36,7 @@ public class BoardState: MonoBehaviour
 
     public bool Place_Stone(Vector3Int target, int input_player)
     { // player: 0=white, 1=black
-        int boardSize = GameObject.FindWithTag("GameController").GetComponent<BoardState>().board_size;
+        int boardSize = getBoardSize();
         int max = boardSize / 2;
         int min = -max;
         if (target.x < min || target.x > max || target.y < min || target.y > max)
@@ -62,18 +63,40 @@ public class BoardState: MonoBehaviour
 
     }
 
-    // private Vector2Int quantize_input(Vector3Int input)
-    // {
-    //     Vector2Int target = new()
-    //     {
-    //         y = (int)MathF.Round(input.y),
-    //         x = (int)MathF.Round(input.x)
-    //     };
+    public int getBoardSize()
+    {
+        return _BoardSizes[_board_size];
+    }
 
-    //     return target;
-    // }
+    public bool increaseBoardSize()
+    {
+        if (_board_size < 3)
+        {
+            _board_size++;
+            Debug.Log("Increased board size");
+            return true;
+        }
+        else
+        {
+            Debug.Log("Cant increase board size past 31");
+            return false;
+        }
+    }
 
-
+    public bool reduceBoardSize()
+    {
+        if (_board_size > 1)
+        {
+            _board_size--;
+            Debug.Log("Reduced board size");
+            return true;
+        }
+        else
+        {
+            Debug.Log("Cant reduce board size lower than 9");
+            return false;
+        }
+    }
 
     private void invalid_selection()
     {
---
+++ Assets/Scripts/Powerups/ExpandBoard.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnlargeBoard : MonoBehaviour
+{
+    public Dictionary<Vector3Int, BoardCell> Expand(
+        Dictionary<Vector3Int, BoardCell> input, int current_player)
+    {
+        BoardState board = GameObject.Find("GameController").GetComponent<BoardState>();
+        GridBuilder grid = GameObject.Find("Board_Tilemap").GetComponent<GridBuilder>();
+        Camera camera    = GameObject.Find("Main Camera").GetComponent<Camera>();
+
+        board.increaseBoardSize();
+        grid.redrawGrid();
+        camera.orthographicSize = 17;
+        return input;
+    }
+
+
+
+    public void testEnlarge()
+    {
+        Debug.Log("Expand Dong");
+        Dictionary<Vector3Int, BoardCell> board_state = GameObject.Find("GameController").GetComponent<BoardState>().board_state;
+        GameObject.Find("GameController").GetComponent<BoardState>().board_state = Expand(board_state, 0);
+        GameObject.Find("Stone_Tilemap").GetComponent<DrawStones>().redraw_stones();
+    }
+
+
+}
